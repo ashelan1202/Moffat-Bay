6f3c3a69ba77430188ca9d011a51c32a1066a018
Rogelio Orozco <roji19045@gmail.com>
2024-09-04 01:31:05 -0700

    Created Landing page. Updated TestPageLinks to include Landing page. In template.css, updated Footer height from auto to 150px so that I could add a bottom-padding to my landing page and prevent it from being obscured.

--- Views/Assets/links/TestPageLinks.php
+++ Views/Assets/links/TestPageLinks.php
@@ -2,5 +2,6 @@
 $pages=[
     "Customer Test" => views."TestPages/CustomerTest.php",
     "Test Page" => views."TestPages/test.php",
+    "Landing Page" => views."TestPages/landing.php",
     ];
 $home = $pages["Customer Test"];
