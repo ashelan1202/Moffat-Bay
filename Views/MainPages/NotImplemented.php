253ae3517de442340d5ecee47551a8281126183b
SamSegars <sam.segars@yahoo.com>
2024-09-07 19:16:25 -0400

    Fixed links,ensured footer is always at bottom of page

--- Views/MainPages/NotImplemented.php
+++ Views/MainPages/NotImplemented.php
@@ -1,9 +1,26 @@
 <?php
 //Delete when All Pages are fully implemented
+$repRoot = "../../";
+$title = "NotImplemented";
 require_once "../templates/_header.php";
+?>
+<style>
+    body{text-align: center}
+</style>
+<?php
+
 require_once "../templates/_navbar.php";
 
 ?>
+
 <h1>This Page has not been implemented</h1>
+<h2>Links</h2>
+<?php
+    foreach($pages as $key => $value){
+    if($key != $title){
+    echo "<a href=\"".$value."\"><p>".$key."</p></a>";
+    }
+    }
+?>
 <?php
 require_once "../templates/_footer.php";
