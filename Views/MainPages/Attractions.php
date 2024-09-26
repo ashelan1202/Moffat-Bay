f85633deaa9dc6ac9e2215cbb0ca0f4af31760c6
Ash Landin <alandin@my365.bellevue.edu>
2024-09-26 11:24:26 -0700

    Making changes to attractions page, increasing size of font for better readability

--- Views/MainPages/Attractions.php
+++ Views/MainPages/Attractions.php
@@ -17,11 +17,11 @@ require_once "../templates/_navbar.php";
         </div>
         <div class="section-text">
             <h1>Attractions</h1>
-            <h3>Moffat Bay Lodge invites guests to explore a variety of local attractions.</h3>
+            <h2>Moffat Bay Lodge invites guests to explore a variety of local attractions.</h2>
 
-            <h3>From exciting whale-watching tours and serene kayaking trips, to guided hikes through untouched wilderness and captivating scuba diving excursions.</h3>
+            <h2>From exciting whale-watching tours and serene kayaking trips, to guided hikes through untouched wilderness and captivating scuba diving excursions.</h2>
 
-            <h3>Whether seeking adventure or tranquility, visitors will find countless ways to connect with the natural beauty and wonder of Moffat Bay. </h3>
+            <h2>Whether seeking adventure or tranquility, visitors will find countless ways to connect with the natural beauty and wonder of Moffat Bay. </h2>
         </div>
     </div>
 
@@ -31,9 +31,9 @@ require_once "../templates/_navbar.php";
         </div>
         <div class="section-text">
             <h1>Scuba Diving</h1>
-            <h3>Explore the underwater world with scuba diving! Plunge into crystal-clear waters, navigate coral reefs, and meet the resident marine life for an adventure like no other.</h3>
-            <h3>Our guided tours offer a journey through the bay's hidden wonders, making it a must-try experience for anyone who loves the ocean. </h3>
-            <h3>Beginner divers welcome!</h3>
+            <h2>Explore the underwater world with scuba diving! Plunge into crystal-clear waters, navigate coral reefs, and meet the resident marine life for an adventure like no other.</h2>
+            <h2>Our guided tours offer a journey through the bay's hidden wonders, making it a must-try experience for anyone who loves the ocean. </h2>
+            <h2>Beginner divers welcome!</h2>
         </div>
     </div>
 
@@ -44,8 +44,8 @@ require_once "../templates/_navbar.php";
         </div>
         <div class="section-text">
             <h1>Whale Watching</h1>
-            <h3>Embark on an unforgettable whale watching voyage at the nearby Moffat Bay Marina.</h3>
-            <h3>Witness majestic whales as they glide through our pristine waters, offering a front-row seat to one of nature's most awe-inspiring spectacles. </h3>
+            <h2>Embark on an unforgettable whale watching voyage at the nearby Moffat Bay Marina.</h2>
+            <h2>Witness majestic whales as they glide through our pristine waters, offering a front-row seat to one of nature's most awe-inspiring spectacles. </h2>
 
         </div>
     </div>
@@ -56,8 +56,8 @@ require_once "../templates/_navbar.php";
         </div>
         <div class="section-text">
             <h1>Kayaking</h1>
-            <h3>Paddle through the calm waters to explore Moffat Bay's secluded coves, lush shoreline and diverse marine life. Kayaking at Moffat Bay offers the perfect blend of peaceful escape and exploration.</h3>
-            <h3>Book your kayak adventure today and immerse yourself in the natural wonders that surround our lodge.  </h3>
+            <h2>Paddle through the calm waters to explore Moffat Bay's secluded coves, lush shoreline and diverse marine life. Kayaking at Moffat Bay offers the perfect blend of peaceful escape and exploration.</h2>
+            <h2>Book your kayak adventure today and immerse yourself in the natural wonders that surround our lodge.  </h2>
         </div>
     </div>
 
@@ -68,9 +68,9 @@ require_once "../templates/_navbar.php";
         </div>
         <div class="section-text">
             <h1>Hiking</h1>
-            <h3>Encounter the untamed beauty of Joviedsa Island with our guided hiking adventure at Moffat Bay Lodge.</h3>
-            <h3>Traverse trails that wind through ancient forests, along rugged coastline, and up to breathtaking viewpoints overlooking the Bay. </h3>
-            <h3>Each step reveals the island's rich natural landscape, from towering trees and vibrant wildflowers, to chance encounters with local wildlife.</h3>
+            <h2>Encounter the untamed beauty of Joviedsa Island with our guided hiking adventure at Moffat Bay Lodge.</h2>
+            <h2>Traverse trails that wind through ancient forests, along rugged coastline, and up to breathtaking viewpoints overlooking the Bay. </h2>
+            <h2>Each step reveals the island's rich natural landscape, from towering trees and vibrant wildflowers, to chance encounters with local wildlife.</h2>
         </div>
     </div>
 
